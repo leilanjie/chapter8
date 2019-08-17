ede13b7a72815897af690cd5711deccb0eac7c3a
leilanjie <lanjie.lei@neoway.com>
2019-08-17 13:15:40 +0800

    练习1.1

--- src/test/java/com/smart/dao/BaseDaoTest.java
+++ src/test/java/com/smart/dao/BaseDaoTest.java
@@ -3,7 +3,13 @@ package com.smart.dao;
 import org.unitils.UnitilsTestNG;
 import org.unitils.spring.annotation.SpringApplicationContext;
 
+import java.sql.SQLOutput;
+
 @SpringApplicationContext({"xiaochun-dao.xml"})
-public class BaseDaoTest extends UnitilsTestNG{
-	
+public class BaseDaoTest extends UnitilsTestNG {
+
+    public static void main(String[] args){
+        System.out.println("lalala");
+    }
+
 }
